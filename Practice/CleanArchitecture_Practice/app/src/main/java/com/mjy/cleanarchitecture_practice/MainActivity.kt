95322c9ac399e209b791bc5fad4d1c0482aabbbf
jy-mun97 <mjy0750@gmail.com>
2022-05-23 20:55:26 +0900

    Initial commit

---
+++ Practice/CleanArchitecture_Practice/app/src/main/java/com/mjy/cleanarchitecture_practice/MainActivity.kt
@@ -0,0 +1,11 @@
+package com.mjy.cleanarchitecture_practice
+
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+    }
+}
